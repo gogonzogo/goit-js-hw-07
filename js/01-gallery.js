68eef519524ee590861aee9d0e15ada6a196fb19
Jose Gonzales <jose.gonzales08@ymail.com>
2023-01-26 23:00:22 -0500

    task 1 update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+"use strict";
 
-console.log(galleryItems);
+const ref = {
+    galleryList: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = galleryItems.map((item) =>
+    `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="item.preview" alt="${item.description}/></a></div>"`).join('');
+
+ref.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+const galleryImage = document.querySelector(".gallery__link");
+console.log(galleryImage);
+const largeImage = galleryImage.getAttribute("href");
+console.log(largeImage);
+
+ref.galleryList.addEventListener("click", (handleClick));
+
+function handleClick(e) {
+    e.preventDefault();
+    if (e.target === largeImage) {
+        console.log("Responding");
+    }
+}
+
+import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
+
