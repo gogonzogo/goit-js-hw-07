165ed4582ec9c72349c5c8c60ffe998ab6cf6721
Jose Gonzales <jose.gonzales08@ymail.com>
2023-02-06 08:56:37 -0500

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,45 +10,43 @@ const ref = {
 // MARKUP FUNCTION
 function galleryMarkup(array) {
     return array.map((item) =>
-    `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="item.preview" alt="${item.description}/></a></div>"`).join('');
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}" 
+        data-source="${item.original}" 
+        alt="${item.description}" width="340"/></a></div>`).join('');
 }
 
 ref.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(galleryItems));
 
-// HTML REFRENCES
-const galleryImage = document.querySelector(".gallery__link");
-const largeImage = galleryImage.getAttribute("href");
-const originalImage = document.querySelector('.gallery__image')
+// EVENT LISTENER AND FUNCTION
+ref.galleryList.addEventListener("click", handleClick);
 
-
-// LIGHTBOX FUNCTIONS
-ref.galleryList.addEventListener("click", (handleClick));
-
-const imageSelect = basicLightbox.create(`
-    <img width="800" height="600"
-    src="${galleryImage}">
-`)
-
-const lightboxVisible = imageSelect.visible();
-
-// SHOW LIGHTBOX
 function handleClick(e) {
     e.preventDefault();
-    if (e.target === originalImage) {
-        console.log(handleClick);
-        imageSelect.show();
-    }
+    const clickTarget = e.target;
+    
+    if (clickTarget.nodeName !== "IMG") {
+        return;
+    };
+
+    const selectedImage = basicLightbox.create(`
+    <img width= 650" height="450"
+    src="${clickTarget.dataset.source}">
+`);
+    selectedImage.show();
+
+    // ESC CLOSE LIGTHBOX
+    ref.galleryList.addEventListener("keydown", (handleEscPress));
+
+    function handleEscPress(e) {
+        if (e.key === 'Escape') {
+            selectedImage.close();
+        }
+    };
+
 }
-
-// ESC CLOSE LIGTHBOX
-ref.galleryList.addEventListener("keydown", (handleEscPress));
-
-function handleEscPress(e) {
-    if (e.key === 'Escape') {
-        imageSelect.close();
-    }
-}
-
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
+// import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+"use strict";
+
+// HTML REFRENCES
+const ref = {
+    galleryList: document.querySelector('.gallery'),
+};
+
+// MARKUP FUNCTION
+function galleryMarkup(array) {
+    return array.map((item) =>
+        `<a class="gallery__link" href="${item.original}"><img class="gallery__image"src="${item.preview}" alt="${item.description}" width="340"/></a>`).join('');
+}
+
+ref.galleryList.insertAdjacentHTML("afterbegin", galleryMarkup(galleryItems));
+
+// EVENT LISTENER AND FUNCTION
+ref.galleryList.addEventListener("click", handleClick);
+
+function handleClick(e) {
+    e.preventDefault();
+    const clickTarget = e.target;
+    console.log(clickTarget.nodeName);
+
+    if (clickTarget.nodeName !== "IMG") {
+        return;
+    };
+
+    let showLightbox = new simpleLightbox('.gallery a');
+    
+        showLightbox.on('show.simplelightbox', function () {
+            
+        });
+    
+
+    // ESC CLOSE LIGTHBOX
+    ref.galleryList.addEventListener("keydown", (handleEscPress));
+
+    function handleEscPress(e) {
+        if (e.key === 'Escape') {
+            
+        }
+    };
+
+}
